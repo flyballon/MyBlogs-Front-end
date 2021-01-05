675a22fb211b0865b9c19e59862134d3dc5e5072
flyballon <ykf727634832@gmail.com>
2021-01-05 16:07:49 +0800

    element-ui配置

---
+++ src/plugins/element-ui.js
@@ -0,0 +1,10 @@
+import { Button, Select } from 'element-ui'
+
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+/* 或写为
+ * Vue.use(Button)
+ * Vue.use(Select)
+ */
+
+export default element-ui
